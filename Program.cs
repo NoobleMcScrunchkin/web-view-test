b2d9c6eab86699e892fe931291616321e8178107
NoobleMcScrunchkin <kieranaslett@gmail.com>
2023-07-08 17:31:29 +0100

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 using WinFormsApp;
 
+// Development
+// dotnet run
+
+// Publishing
+// dotnet restore "./web-view-test.csproj" --runtime win-x64
+// dotnet publish "web-view-test.csproj" -c Release -o ./publish --no-restore --runtime win-x64 --self-contained true /p:PublishSingleFile=true
+
 namespace web_view_test
 {
     public class Program
