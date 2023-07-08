0e4f5dd4a85fddb0573bdef0e802b08982858ae0
NoobleMcScrunchkin <kieranaslett@gmail.com>
2023-07-08 17:28:31 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using WinFormsApp;
+
+namespace web_view_test
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().RunAsync();
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
